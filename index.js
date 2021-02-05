4344468db2fef8fa114b651cbdb77ebf4a7efc60
mamiya9738 <gogo9738@gmail.com>
2021-02-05 22:26:47 +0900

    スマホ拡大禁止やり方変更

--- index.js
+++ index.js
@@ -121,3 +121,23 @@ const nextClick = () => {
         document.getElementById('next').textContent = "END";
     }
 }
+
+/* "passive" が使えるかどうかを検出 */
+let passiveSupported = false;
+try {
+    document.addEventListener("test", null, Object.defineProperty({}, "passive", {
+        get: function() {
+            passiveSupported = true;
+        }
+    }));
+} catch(err) {}
+
+/* リスナーを登録 */
+document.addEventListener('touchstart', function listener(e) {
+    /* do something */
+    e.preventDefault();
+}, passiveSupported ? { passive: false } : false);
+document.addEventListener('touchmove', function listener(e) {
+    /* do something */
+    e.preventDefault();
+}, passiveSupported ? { passive: false } : false);
